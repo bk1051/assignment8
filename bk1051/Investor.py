de3d24dbe26f6f3e791b634af2902bff494b882c
Brian Karfunkel <bk1051@nyu.edu>
2015-11-07 15:01:06 -0500

    making structure

---
+++ bk1051/Investor.py
@@ -0,0 +1,14 @@
+
+
+class Investor(object):
+
+	def __init__(self, positions, num_trials):
+		self.positions = positions
+		self.num_trials = num_trials
+		self.initial_budget = 1000
+
+	def run_simulations(self):
+		pass
+
+	def plot():
+		pass
---
+++ bk1051/Simulator.py
@@ -0,0 +1,10 @@
+
+
+class Simulator(object):
+
+
+	def __init__(self, position, budget):
+		self.position_value = budget / position
+
+	def run(self):
+		pass
